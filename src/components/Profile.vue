7aac403acd18b9e441d79986a031c920c6939d59
racheliver <racheliver@gmail.com>
2024-07-07 00:11:10 +0300

    profile html

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,30 +1,31 @@
 <template>
-    <Container>
-      <div class="profile-container">
-            <h1>USERBAR</h1>
-            <h1>IMAGEGALERY</h1>
-      </div>
-    </Container>
-  </template>
-  
-  <script setup>
-  import Container from "./Container.vue";
-  import Card from "./Card.vue";
+  <Container>
+    <div class="profile-container">
+      <UserBar
+        :username="'laithharb'"
+        :userinfo="{ posts: 4, followers: 4, following: 342 }"
+      />
+      <h1>IMAGEGALERY</h1>
+    </div>
+  </Container>
+</template>
 
-  </script>
-  
-  <style scoped>
-  .profile-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px 0;
-  }
-  .post-image {
-    width: 100%;
-    max-width: 640px;
-    height: auto;
-    margin-bottom: 1em;
-  }
-  </style>
-  
+<script setup>
+import Container from "./Container.vue";
+import UserBar from "./UserBar.vue";
+</script>
+
+<style scoped>
+.profile-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 0;
+}
+.post-image {
+  width: 100%;
+  max-width: 640px;
+  height: auto;
+  margin-bottom: 1em;
+}
+</style>
---
+++ src/components/UserBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="userbar-container">
+        <div class="top-content">
+            <a-typography-title :level="2">{{ props.username }}</a-typography-title>
+        </div>
+        <div class="bottom-content">
+            <a-typography-title :level="5">{{ props.userinfo.posts }} posts</a-typography-title>
+            <a-typography-title :level="5">{{props.userinfo.followers}} followers</a-typography-title>
+            <a-typography-title :level="5">{{props.userinfo.following}} following</a-typography-title>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import {defineProps} from 'vue';
+    const props = defineProps(['username', 'userinfo']);
+</script>
+
+<style scoped>
+.userbar-container{
+    display: table-column;
+    padding-bottom: 75px;
+}
+.bottom-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+
+.bottom-content h5{
+    margin: 0;
+    margin-right: 30px;
+}
+</style>
